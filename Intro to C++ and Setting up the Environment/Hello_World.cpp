b772ab74c05c26ceec674ab539e1251be2fc9358
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-04-07 12:21:41 +0530

    Intro to C++ and Setting up the Environment

---
+++ Intro to C++ and Setting up the Environment/Hello_World.cpp
@@ -0,0 +1,21 @@
+
+// ======================================================
+//  Author       : spexcher | Gourab Modak
+//  Created      : 07.04.2026 at 12:18:50
+//  Contact      : spexcher@gmail.com
+//  Linktree     : https://linktr.ee/spexcher/
+//  GitHub       : https://github.com/spexcher/
+//  LinkedIn     : https://www.linkedin.com/in/gourabmodak/
+//  YouTube      : https://www.youtube.com/@spexcher
+//
+//  Happy Coding! Build, learn, and ship with confidence.
+// ======================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    cout << "Hello World\n";
+    return 0;
+}
