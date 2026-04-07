7274f178b6af82dcb5bff9a8237683e52b48af11
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-04-07 12:25:57 +0530

    Data Types, Variables and Input Output completed

---
+++ B_Basic_Data_Types.cpp
@@ -0,0 +1,31 @@
+
+// ======================================================
+//  Author       : spexcher | Gourab Modak
+//  Created      : 07.04.2026 at 12:25:24
+//  Contact      : spexcher@gmail.com
+//  Linktree     : https://linktr.ee/spexcher/
+//  GitHub       : https://github.com/spexcher/
+//  LinkedIn     : https://www.linkedin.com/in/gourabmodak/
+//  YouTube      : https://www.youtube.com/@spexcher
+//
+//  Happy Coding! Build, learn, and ship with confidence.
+// ======================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int a;
+    ll b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << "\n"
+         << b << "\n"
+         << c << "\n"
+         << d << "\n"
+         << e << "\n";
+    return 0;
+}
