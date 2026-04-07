65e2e254249d99a91e3e8984f2c3902fa16aebbc
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-04-07 12:33:52 +0530

    renamed:    Intro to C++ and Setting up the Environment/.cph/.Hello_World.cpp_fad28aceda3d04f1fda7d57f519f6cef.prob -> 1.Intro to C++ and Setting up the Environment/.cph/.Hello_World.cpp_fad28aceda3d04f1fda7d57f519f6cef.prob 	renamed:    Intro to C++ and Setting up the Environment/Hello_World.cpp -> 1.Intro to C++ and Setting up the Environment/Hello_World.cpp 	renamed:    Data Types, Variables and Input Output/B_Basic_Data_Types.cpp -> 2.Data Types, Variables and Input Output/B_Basic_Data_Types.cpp 	new file:   3.Operators/.cph/.C_Simple_Calculator.cpp_8ab6a344a9188c593aa47157fce53c0e.prob 	new file:   3.Operators/.cph/.D_Difference.cpp_70b9736df1449266c707212f656e0fe4.prob 	new file:   3.Operators/C_Simple_Calculator.cpp 	new file:   3.Operators/D_Difference.cpp

---
+++ 3.Operators/C_Simple_Calculator.cpp
@@ -0,0 +1,25 @@
+
+// ======================================================
+//  Author       : spexcher | Gourab Modak
+//  Created      : 07.04.2026 at 12:31:46
+//  Contact      : spexcher@gmail.com
+//  Linktree     : https://linktr.ee/spexcher/
+//  GitHub       : https://github.com/spexcher/
+//  LinkedIn     : https://www.linkedin.com/in/gourabmodak/
+//  YouTube      : https://www.youtube.com/@spexcher
+//
+//  Happy Coding! Build, learn, and ship with confidence.
+// ======================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << (a + b) << endl;
+    cout << a << " * " << b << " = " << (a * b) << endl;
+    cout << a << " - " << b << " = " << (a - b) << endl;
+    return 0;
+}
---
+++ 3.Operators/D_Difference.cpp
@@ -0,0 +1,23 @@
+
+// ======================================================
+//  Author       : spexcher | Gourab Modak
+//  Created      : 07.04.2026 at 12:32:53
+//  Contact      : spexcher@gmail.com
+//  Linktree     : https://linktr.ee/spexcher/
+//  GitHub       : https://github.com/spexcher/
+//  LinkedIn     : https://www.linkedin.com/in/gourabmodak/
+//  YouTube      : https://www.youtube.com/@spexcher
+//
+//  Happy Coding! Build, learn, and ship with confidence.
+// ======================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << "Difference = " << (a * b) - (c * d) << endl;
+    return 0;
+}
